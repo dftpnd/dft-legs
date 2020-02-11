16c550fab29cc36395fa9ea5450264f82d53ed37
bb8 <lkdnvc@gmail.com>
2020-02-11 19:38:13 +0300

    work serial examplw

--- index.js
+++ index.js
@@ -10,14 +10,28 @@ device.findSerialPortChannel(address, function(channel){
   // make bluetooth connect to remote device
   bluetooth.connect(address, channel, function(err, connection){
     if(err) return console.error(err);
-	
-    const data = ['a', 'b'];
-    const data2 = JSON.stringify(data);
-    const data3 = new Buffer(data2, 'utf-8');
+
+    const data0 = "{\"sensor\":\"gps\",\"time\":1351824120,\"data\":[48.756080,2.302038]}";
+    const data1 = {
+	   M1: 100,
+	   M2: 250
+    };
+    const data2 = JSON.stringify(data1);//.split('').map((char)=> char === '"' ? '\"': char).join('');
+    console.log('data2', data2);
+
+    const data3 = Buffer.from(data2, 'utf-8');
+
+    console.log('data3',data3)
 
     connection.write(data3, (all) => {
       console.log("wrote", all);
     });
+
+
+
+    connection.on('data', buffer => {
+    	console.log('recived message:', buffer.toString(), buffer)
+    })
   });
 
 });
